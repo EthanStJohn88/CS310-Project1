f3ca5c1c8cf93747af8f391b9263d1367d63c084
Charlie Owen <cowen6@stu.jus.edu>
2022-02-25 13:24:22 -0600

    Created and Tested Badge Class

---
+++ src/edu/jsu/mcis/cs310/tas_sp22/Badge.java
@@ -0,0 +1,32 @@
+package edu.jsu.mcis.cs310.tas_sp22;
+
+import java.util.HashMap;
+ 
+public class Badge {
+    
+    private String id, description;
+
+    public Badge(HashMap<String, String> params){
+        this.id = params.get("id");
+        this.description = params.get("description");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        
+        result.append("#").append(id).append(" (").append(description).append(")");
+        
+        return result.toString();
+    }
+    
+    
+}
--- src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
+++ src/edu/jsu/mcis/cs310/tas_sp22/TAS.java
@@ -1,10 +1,23 @@
 package edu.jsu.mcis.cs310.tas_sp22;
 
+import java.util.HashMap;
+
 public class TAS {
 
     public static void main(String[] args) {
         final String username = "tasuser";
         final String password = "Team_A";
+        
+        //Badge Test
+        HashMap<String, String> badge = new HashMap<>();
+        badge.put("description", "Chapman, Joshua E");
+        badge.put("id", "12565C60");
+        
+        Badge b1 = new Badge(badge);
+        System.err.println(badge);
+        System.err.println(b1.getId());
+        System.err.println(b1.getDescription());
+        System.err.println(b1.toString());
     }
     
 }
